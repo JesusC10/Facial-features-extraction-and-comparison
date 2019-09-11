48e117c8c535fcd797424d1a7ebd0bfa7056b20a
yannlelorier <31284120+yannlelorier@users.noreply.github.com>
2019-09-11 09:31:08 -0500

    Add files via upload
    Cmake, csv y main agregado

---
+++ main.cpp
@@ -0,0 +1,81 @@
+#include "opencv2/core.hpp"
+#include "opencv2/face.hpp"
+#include "opencv2/highgui.hpp"
+
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace cv;
+using namespace cv::face;
+using namespace std;
+
+static void read_csv(const string& facesPath, vector<Mat>& images, vector<int>& labels, char separator = ';') {
+    std::ifstream file(facesPath.c_str(), ifstream::in);
+    if (!file) {
+        string error_message = "No valid input file was given, please check the given filename.";
+        CV_Error(Error::StsBadArg, error_message);
+    }
+    string line, path, classlabel;
+    while (getline(file, line)) {
+        stringstream liness(line);
+        getline(liness, path, separator);
+        getline(liness, classlabel);
+        if(!path.empty() && !classlabel.empty()) {
+            images.push_back(imread(path, 0));
+            labels.push_back(atoi(classlabel.c_str()));
+        }
+    }
+}
+
+int main(int argc, const char *argv[]){
+//    if (argc != 2) {
+//        cout << "usage: " << argv[0] << " <csv.ext>" << endl;
+//        exit(1);
+//    }
+    // Get the path to your CSV.
+    //i.e. : ~/CLionProjects/FeatureExtraction
+    string fn_csv = string(argv[1]);
+    // These vectors hold the images and corresponding labels.
+    vector<Mat> images;
+    vector<int> labels;
+    try {
+        read_csv(fn_csv, images, labels);
+    } catch (const cv::Exception& e) {
+        cerr << "Error opening file \"" << fn_csv << "\". Reason: " << e.msg << endl;
+        // nothing more we can do
+        exit(1);
+    }
+
+    Mat testSample = images[images.size() - 1];
+    int testLabel = labels[labels.size() - 1];
+    images.pop_back();
+    labels.pop_back();
+
+    Ptr<LBPHFaceRecognizer> model = LBPHFaceRecognizer::create();
+    model->train(images, labels);
+
+    int predictedLabel = model->predict(testSample);
+
+    string result_message = format("Predicted class = %d / Actual class = %d.", predictedLabel, testLabel);
+    cout << result_message << endl;
+
+    model->setThreshold(0.0);
+
+    predictedLabel = model->predict(testSample);
+    cout << "Predicted class = " << predictedLabel << endl;
+
+    cout << "Model Information:" << endl;
+    string model_info = format("\tLBPH(radius=%i, neighbors=%i, grid_x=%i, grid_y=%i, threshold=%.2f)",
+                               model->getRadius(),
+                               model->getNeighbors(),
+                               model->getGridX(),
+                               model->getGridY(),
+                               model->getThreshold());
+    cout << model_info << endl;
+    vector<Mat> histograms = model->getHistograms();
+    // But should I really visualize it? Probably the length is interesting:
+    cout << "Size of the histograms: " << histograms[0].total() << endl;
+    return 0;
+}
